1e7560fe2b9138a4aa06e5a712a8ff05ce3e3e14
Arjan Singh Bal <arjan.singh@codenation.co.in>
2020-06-11 16:17:55 +0530

    added first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: 'post-create.component.html'
+})
+
+export class PostCreateComponent {
+
+}
