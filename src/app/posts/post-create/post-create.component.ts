39685cd912b7ca9d5ebd2dbd08dd2aa9583e9321
Arjan Singh Bal <arjan.singh@codenation.co.in>
2020-06-12 14:09:22 +0530

    Added property and event binding to create posts

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
@@ -7,10 +7,15 @@ import { Component } from '@angular/core';
 })
 
 export class PostCreateComponent {
-  newPost = 'No Content';
-  enteredValue = '';
+  enteredTitle = '';
+  enteredContent = '';
+  @Output() postCreated = new EventEmitter();
 
   onAddPost() {
-    this.newPost = this.enteredValue;
+    const post = {
+      title: this.enteredTitle,
+      content: this.enteredContent
+    };
+    this.postCreated.emit(post);
   }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
@@ -12,5 +12,5 @@ export class PostListComponent {
   //   {title: 'Second Post', content: 'This is the second post'},
   //   {title: 'Third Post', content: 'This is the third post'}
   // ];
-  posts = [];
+  @Input() posts = [];
 }
