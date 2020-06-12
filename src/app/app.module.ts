a3f2dc88183cb9504a438b4a3318e143dc217d7f
Arjan Singh Bal <arjan.singh@codenation.co.in>
2020-06-12 12:49:56 +0530

    added expansion panels for post list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,19 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule, MatToolbar } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +27,8 @@ import { HeaderComponent } from './header/header.component';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent {
+  posts = [
+    {title: 'First Post', content: 'This is the first post'},
+    {title: 'Second Post', content: 'This is the second post'},
+    {title: 'Third Post', content: 'This is the third post'}
+  ];
+}
