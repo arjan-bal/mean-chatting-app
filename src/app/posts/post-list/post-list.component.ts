36ac9e53d912a826a5d6707e6ec0fac1b19d734e
Arjan Singh Bal <arjan.singh@codenation.co.in>
2020-06-12 16:45:48 +0530

    Created post service for dependency injection

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 import { Post } from '../post.model'
+import { PostService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
@@ -15,4 +16,6 @@ export class PostListComponent {
   //   {title: 'Third Post', content: 'This is the third post'}
   // ];
   @Input() posts: Post[] = [];
+
+  constructor(public PostsService: PostService) {}
 }
---
+++ src/app/posts/posts.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+import { Post } from './post.model';
+
+@Injectable({providedIn: 'root'})
+export class PostService {
+  private posts: Post[] = [];
+
+  getPosts() {
+    return [...this.posts];
+  }
+
+  addPost(title: string, content: string) {
+    const post: Post = {title: title, content: content};
+    this.posts.push(post);
+  }
+}
