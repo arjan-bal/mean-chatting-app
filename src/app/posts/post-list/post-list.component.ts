3d1141645940f847378844424a836ea9b13cdcd0
Arjan Singh Bal <arjan.singh@codenation.co.in>
2020-06-13 12:42:56 +0530

    Added observables from rxjs for getPosts

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model'
 import { PostsService } from '../posts.service';
@@ -9,17 +10,26 @@ import { PostsService } from '../posts.service';
   styleUrls: ['./post-list.component.css']
 })
 
-export class PostListComponent implements OnInit{
+export class PostListComponent implements OnInit, OnDestroy{
   // posts = [
   //   {title: 'First Post', content: 'This is the first post'},
   //   {title: 'Second Post', content: 'This is the second post'},
   //   {title: 'Third Post', content: 'This is the third post'}
   // ];
   posts: Post[] = [];
+  private postsSub: Subscription;
 
   constructor(public PostsService: PostsService) {}
 
   ngOnInit() {
     this.posts = this.PostsService.getPosts();
+    this.postsSub = this.PostsService.getPostUpdateListner()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
   }
 }
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 import { Post } from './post.model';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
   private posts: Post[] = [];
+  private postsUpdated: Subject<Post[]> = new Subject<Post[]>();
 
   getPosts() {
-    return this.posts;
+    return [...this.posts];
+  }
+
+  getPostUpdateListner() {
+    return this.postsUpdated.asObservable();
   }
 
   addPost(title: string, content: string) {
     const post: Post = {title: title, content: content};
     this.posts.push(post);
+    this.postsUpdated.next([...this.posts]);
   }
 }
