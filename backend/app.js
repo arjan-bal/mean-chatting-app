5194cadb108d387b0bee1b2cec7b583df3043788
Arjan Singh Bal <arjan.singh@codenation.co.in>
2020-06-13 16:59:39 +0530

    returned dummy posts from node server

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: '3dsfds2342s',
+      title: 'post 1',
+      content: 'this is some good content'
+    },
+    {
+      id: '3dsfio21213',
+      title: 'post 2',
+      content: 'this is some good content two'
+    }
+  ];
+  res.status(200).json({
+    message: 'posts send successfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
