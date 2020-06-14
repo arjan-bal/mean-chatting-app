e87c8df5120c22a37bb90f135b0d78d283b6532b
Arjan Singh Bal <arjan.singh@codenation.co.in>
2020-06-14 17:27:50 +0530

    fetching posts from db and returning in get request

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const Post = require('./models/post');
-const { title } = require('process');
 
 const app = express();
 
@@ -54,21 +53,12 @@ app.post('/api/posts', (req, res, next) => {
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: '3dsfds2342s',
-      title: 'post 1',
-      content: 'this is some good content'
-    },
-    {
-      id: '3dsfio21213',
-      title: 'post 2',
-      content: 'this is some good content two'
-    }
-  ];
-  res.status(200).json({
-    message: 'posts sent successfully',
-    posts: posts
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'posts sent successfully',
+        posts: documents
+      });
   });
 });
 
