3e7a2dce52fd5403d889f8bbf2a9be425be6a894
Arjan Singh Bal <arjan3141@gmail.com>
2020-06-17 12:41:42 +0530

    changed promises to async await in posts routes

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -6,62 +6,56 @@ const router = express.Router();
 
 // /api/posts is already present in path due to app.js
 
-router.post('', (req, res, next) => {
+router.post('', async (req, res, next) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content
   });
   // console.log(post);
-  post.save().then((createdPost) => {
-    res.status(201).json({
-      message: "post added successfully",
-      postId: createdPost._id
-    });
+  const createdPost = await post.save();
+  res.status(201).json({
+    message: "post added successfully",
+    postId: createdPost._id
   });
 });
 
-router.get('', (req, res, next) => {
-  Post.find()
-    .then(documents => {
-      res.status(200).json({
-        message: 'posts sent successfully',
-        posts: documents
-      });
-  });
+router.get('', async (req, res, next) => {
+  const documents = await Post.find();
+   res.status(200).json({
+      message: 'posts sent successfully',
+      posts: documents
+    });
 });
 
 // get single post for Update
-router.get('/:id', (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post){
-      res.status(200).json({
-        message: 'post sent successfully',
-        post: post
-      });
-    } else {
-      res.status(404).json({
-        message: 'post not found!'
-      });
-    }
-  });
-});
-
-router.delete('/:id', (req, res, next) => {
-  Post.deleteOne({_id: req.params.id}).then(result => {
+router.get('/:id', async (req, res, next) => {
+  const post = await Post.findById(req.params.id);
+  if (post){
     res.status(200).json({
-      message: 'Post deleted!'
+      message: 'post sent successfully',
+      post: post
     });
+  } else {
+    res.status(404).json({
+      message: 'post not found!'
+    });
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  const result = await  Post.deleteOne({_id: req.params.id});
+  res.status(200).json({
+    message: 'Post deleted!'
   });
 });
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', async (req, res, next) => {
   const post = {
     title: req.body.title,
     content: req.body.content
   };
-  Post.updateOne({_id: req.params.id}, post).then(result => {
-    res.status(200).json({ message: 'Update successful!' });
-  });
+  const result = await Post.updateOne({_id: req.params.id}, post);
+  res.status(200).json({ message: 'Update successful!' });
 })
 
 module.exports = router;
