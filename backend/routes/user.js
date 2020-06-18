529f046d7aef436e831dfbbf353a5d77989aeaf7
Arjan Singh Bal <arjan3141@gmail.com>
2020-06-18 17:15:25 +0530

    added signup route to backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user');
 
 const router = express.Router();
 
-router.post('/signup', (req, res, next) => {
-
+router.post('/signup', async (req, res, next) => {
+  hash = await bcrypt.hash(req.body.password, 10);
+  const user = new User({
+    email: req.body.email,
+    password: hash
+  });
+  try {
+    result = await user.save();
+    res.status(201).json({
+      message: 'User created!',
+      result: result
+    });
+  } catch(err) {
+    res.status(501).json({
+      error: err
+    });
+  }
 });
 
 module.exports = router;
