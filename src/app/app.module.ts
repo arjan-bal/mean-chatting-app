30a2c0fa26e3547b17a605850d1f56609b22a8a9
Arjan Singh Bal <arjan3141@gmail.com>
2020-06-21 20:32:33 +0530

    changes to readme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDialogModule } from '@angular/material/dialog';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
@@ -43,7 +44,8 @@ import { ErrorInterceptor } from './error-interceptor';
     MatProgressSpinnerModule,
     HttpClientModule,
     MatPaginatorModule,
-    FormsModule
+    FormsModule,
+    MatDialogModule
   ],
   providers: [
     // HTTP_INTERCEPTORS token provided by angular
